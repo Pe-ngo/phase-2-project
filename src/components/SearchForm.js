4cb79f9b025303c19d69d488596f75138abc1821
Pe-ngo <jamespengo95@gmail.com>
2022-07-29 14:35:22 +0300

    Add handleChange and handleSubmit functions

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -4,11 +4,24 @@ import React, { useState } from "react";
 function SearchForm() {
     const [ enterText, setEnterText ] =useState("");
 
+
+    function handleSubmit(e) {
+        e.preventDefault()
+    }
+
+
+    function handleChange(e) {
+        setEnterText({ ...enterText, [e.target.name]:e.target.value});
+      }
+
     return (
             <div className="search-form">
-                <form className="form-text" onSubmit>
-                    <input type="text" placeholder="Search for article"/>
-                    <button type="submit">Search</button>
+                <form className="form-text" onSubmit={handleSubmit}>
+                    <input type="text" 
+                        placeholder="e.g election"
+                        onChange={handleChange}
+                    />
+                    <button type="submit" className="submit-btn">Search</button>
                 </form>
             </div>  
     )
