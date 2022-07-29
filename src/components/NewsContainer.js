8a1509e8069b217c8d09aac51a050d47bd63fb40
Pe-ngo <jamespengo95@gmail.com>
2022-07-29 08:15:15 +0300

    Add contents to the display section

--- src/components/NewsContainer.js
+++ src/components/NewsContainer.js
@@ -12,22 +12,28 @@ function NewsContainer() {
 
     return (
         <div>
+            <div className="display">
+                <div className="display-section">
+                    <h1>View New York Times Articles About Any Topic</h1>
+                </div>
+            </div>
+
             {articles.map(article => {
                 const {abstract, headline: { main }, byline: { original }, 
                     lead_paragraph, news_desk, section_name, web_url, 
                     _id, word_count,} =article
                 return (
-                    <NewsList
-                        key={_id}
-                        abstract={abstract}
-                        main={main}
-                        original={original}
-                        lead_paragraph={lead_paragraph}
-                        news_desk={news_desk}
-                        section_name={section_name}
-                        web_url={web_url}
-                        word_count={word_count}
-                    />
+                        <NewsList
+                            key={_id}
+                            abstract={abstract}
+                            main={main}
+                            original={original}
+                            lead_paragraph={lead_paragraph}
+                            news_desk={news_desk}
+                            section_name={section_name}
+                            web_url={web_url}
+                            word_count={word_count}
+                        />
                 )
             })}
 
--- src/components/NewsList.js
+++ src/components/NewsList.js
@@ -7,20 +7,20 @@ function NewsList({ abstract, main,
                     web_url,word_count, }) {
 
     return (
-        <div className="news-app">
-            <div className="new-list">
-                <h1><a href={web_url}>{main}</a></h1>
-                <h4>{abstract}</h4>
-                <p>{lead_paragraph}</p>
+            <div className="news-app">
+                <div className="new-list">
+                    <h1><a href={web_url}>{main}</a></h1>
+                    <h4>{abstract}</h4>
+                    <p>{lead_paragraph}</p>
 
-                <ul>
-                    <li>{original}</li>
-                    <li>{news_desk}</li>
-                    <li>{section_name}</li>
-                    <li>{word_count}</li>
-                </ul>
-            </div>
-        </div>
+                    <ul>
+                        <li>{original}</li>
+                        <li>{news_desk}</li>
+                        <li>{section_name}</li>
+                        <li>{word_count}</li>
+                    </ul>
+                </div>
+            </div>   
     )
 }
 
